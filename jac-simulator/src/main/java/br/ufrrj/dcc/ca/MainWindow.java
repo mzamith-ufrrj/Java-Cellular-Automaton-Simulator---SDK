d1373c2d3803340335224a11610d26f787076ee7
marcelo <marcelo@tk-85>
2026-02-18 21:41:41 -0300

    2026-02-18 fix bugs

--- jac-simulator/src/main/java/br/ufrrj/dcc/ca/MainWindow.java
+++ jac-simulator/src/main/java/br/ufrrj/dcc/ca/MainWindow.java
@@ -43,6 +43,10 @@ public class MainWindow  extends JFrame {
 		private static final int BOUNDARY = 50;
 		private static final int EXIT_CLOSE				= 0;
 		private static final int ABOUT          		= 1;
+		private static final int ELEMENTARY_CA     		= 2;
+		private static final int GOL_RAND               = 3;
+		private static final int ITENS                  = 4;
+		
 	
 	    private JMenuBar mMenuBar = null;
 	    private JMenu    mMenu    = null,
@@ -50,11 +54,8 @@ public class MainWindow  extends JFrame {
 	    private JMenuItem mMenuItem = null;
 	    
 	    private JDesktopPane mPainel =  null;
-	    
-		private Vector<Integer> mMenuHash = null;
-
-		private Map<String, Internal2DCASIM> mMapCA2D = null;
-
+	    private int []mMenuHash;
+		
 		
 	    
     	public MainWindow() {
@@ -79,15 +80,16 @@ public class MainWindow  extends JFrame {
 			  		so that it can be used in menu option. In this moment, the logical
 			  		part of CA must be instanced
 			 */  
-			mMapCA2D = new HashMap<>();
+			//#AQUI
+			//mMapCA2D = new HashMap<>();
 			
 			
 			//2D Game of life
-			LogicGameOfLife l_gol = new LogicGameOfLife();
-			Internal2DCASIM g_gol = new Internal2DCASIM("Game of Life - rand mode");
-			g_gol.setCAModel(l_gol);
-			mMapCA2D.put(g_gol.getModelName(), g_gol);
-			mMenuHash = new Vector<Integer>();
+			//LogicGameOfLife l_gol = new LogicGameOfLife();
+			//Internal2DCASIM g_gol = new Internal2DCASIM("Game of Life - rand mode");
+			//g_gol.setCAModel(l_gol);
+			//mMapCA2D.put(g_gol.getModelName(), g_gol);
+			//mMenuHash = new Vector<Integer>();
 
 
             this.setContentPane(mPainel);
@@ -100,42 +102,36 @@ public class MainWindow  extends JFrame {
     	
 		 
 		private void createMenu() {
-			mMenuHash.clear();
-            //int index = 0;
-            
-            MenuEvent menuEvent = new MenuEvent();
+			mMenuHash = new int[ITENS];
+        
+			MenuEvent menuEvent = new MenuEvent();
             mMenuBar = new JMenuBar();
             mMenu    = new JMenu("Simulation");
             mMenuBar.add(mMenu);
 			mMenuItem = new JMenuItem("1D - Elementary Cellular Automata");;
 
 			mMenuItem.addActionListener(menuEvent);
-            mMenuHash.add(mMenuItem.hashCode());
+			mMenuHash[ELEMENTARY_CA] = mMenuItem.hashCode();
             mMenu.add(mMenuItem);
 //--------------------------------------------------------------------
             mSubmenu = new JMenu("2D - Cellular Automata");
 
-			
-			for (Map.Entry<String, Internal2DCASIM> entry : mMapCA2D.entrySet()) {
-				String chave = entry.getKey();
-				mMenuItem = new JMenuItem(chave);
-				mMenuItem.addActionListener(menuEvent);
-				mMenuHash.add(mMenuItem.hashCode());
-				mSubmenu.add(mMenuItem);
-			}
-			
+
+			mMenuItem = new JMenuItem("Game of life - random");
+			mMenuItem.addActionListener(menuEvent);
+			mMenuHash[GOL_RAND] = mMenuItem.hashCode();
+			mSubmenu.add(mMenuItem);
             mMenu.add(mSubmenu);
 			
             mMenu.addSeparator();
             mMenuItem = new JMenuItem("Exit");
             mMenuItem.addActionListener(menuEvent);
-            mMenuHash.add(mMenuItem.hashCode());
+			mMenuHash[EXIT_CLOSE] = mMenuItem.hashCode();
             mMenu.add(mMenuItem);
 
             mMenu    = new JMenu("About");
-            mMenuHash.add(mMenuItem.hashCode());
+			mMenuHash[ABOUT] = mMenuItem.hashCode();
             mMenuBar.add(mMenu);
-            
             this.setJMenuBar(mMenuBar);
     	}
     	
@@ -147,69 +143,34 @@ public class MainWindow  extends JFrame {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-				boolean found = false;
-				int i = 0;
-				int option = -1;
-
-				if (e.getSource().hashCode() ==  mMenuHash.get(i)){
+				
+				if (e.getSource().hashCode() ==  mMenuHash[ELEMENTARY_CA]){
 					System.out.println("CALL Elememtary CA");
 					Internal1DCA elementary = new Internal1DCA(mPainel);
 					mPainel.add(elementary);
-					found = true;
+					return;
 				}
-				i++;
 				
-				//Taking into account there are several different models 2D CA
-				while ((i < mMenuHash.size()) && (!found)){
-					Integer opt = mMenuHash.get(i);
-					if (e.getSource().hashCode() == opt){
-                        option = i;
-                        found = true;
-                    }
-					i++;
+				if (e.getSource().hashCode() ==  mMenuHash[GOL_RAND]){
+					LogicGameOfLife l_gol = new LogicGameOfLife();
+					Internal2DCASIM g_gol = new Internal2DCASIM("Game of Life - rand mode");
+					g_gol.setCAModel(l_gol);
+					g_gol.init_window(mPainel);
+					mPainel.add(g_gol);
+					return;
 				}
-				if (option == mMenuHash.size() - 2){
+				
+				if (e.getSource().hashCode() ==  mMenuHash[EXIT_CLOSE]){
 					System.exit(0);
 					return;
 				}
 
-				if (option == mMenuHash.size() - 1){
+				if (e.getSource().hashCode() ==  mMenuHash[ABOUT]){
 					JOptionPane.showMessageDialog(null,"ABOUT","TITULO", JOptionPane.INFORMATION_MESSAGE);
+					
 					return;
 				}
 
-				i = 0;
-				for (Map.Entry<String, Internal2DCASIM> entry : mMapCA2D.entrySet()) {
-					String chave = entry.getKey();
-					Internal2DCASIM valor = entry.getValue();
-					if (option - 1 == i){
-						valor.init_window(mPainel);
-						mPainel.add(valor);
-						System.out.println("Found!");
-						break;
-					}
-					
-				}
-				System.out.println("All your bases are belong to us!");
-				/*
-            	JFileChooser fileChooser = null;
-            	FileNameExtensionFilter filter = null;
-                int option = -1;
-                for (int i = 0; i < ITENS; i++){
-                    if (e.getSource().hashCode() == mMenuHash[i]){
-                        option = i;
-                        i = ITENS + 1;
-                    }
-                }
-                switch (option){
-                
-                	case EXIT_CLOSE:
-                	case ABOUT:JOptionPane.showMessageDialog(null,"ABOUT","TITULO", JOptionPane.INFORMATION_MESSAGE);break;
-                
-                	//case GAME_OF_LIFE:call_game_of_life();break;
-
-                }
-					 */
             }//end-public void actionPerformed(ActionEvent e) {
 
 
